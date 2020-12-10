a6018e7a1b0f0da24242b447ed07af948d6ffa7a
skyenygaard <skyenygaard@gmail.com>
2020-12-10 17:54:18 -0300

    only barebones

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,9 @@ import { ApolloServer } from 'apollo-server';
 
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
 import { createConnection } from 'typeorm';
-// import typeDefs from './model/Schema.graphql';
 import 'reflect-metadata';
 import { buildSchema } from 'type-graphql';
 import { BookResolver } from './resolvers/BookResolver';
-
-// const typeDefs = `
-//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-//
-//   # This "Book" type defines the queryable fields for every book in our data source.
-//   type Book {
-//     title: String
-//     author: String
-//   }
-//
-//   # The "Query" type is special: it lists all of the available queries that
-//   # clients can execute, along with the return type for each. In this
-//   # case, the "books" query returns an array of zero or more Books (defined above).
-//   type Query {
-//     books: [Book]
-//   }
-// `;
-
-// const books = [
-//   {
-//     title: 'The Awakening',
-//     author: 'Kate Chopin',
-//   },
-//   {
-//     title: 'City of Glass',
-//     author: 'Paul Auster',
-//   },
-// ];
 
 async function main() {
   const connection = await createConnection();
@@ -47,18 +18,3 @@ async function main() {
 }
 
 main();
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-// const resolvers = {
-//   Query: {
-//     books: () => books,
-//   },
-// };
-//
-// const server = new ApolloServer({ typeDefs, resolvers });
-//
-// // The `listen` method launches a web server.
-// server.listen().then(({ url }) => {
-//   console.log(`🚀  Server ready at ${url}`);
-// });
