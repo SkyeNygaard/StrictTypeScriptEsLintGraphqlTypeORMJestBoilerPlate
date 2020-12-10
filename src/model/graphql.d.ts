ad2a1f6733e3fe8e4de2222cb90478faad0ca6d8
skyenygaard <skyenygaard@gmail.com>
2020-12-10 17:50:38 -0300

    removed extras

--- src/model/graphql.d.ts
+++
@@ -1,7 +0,0 @@
-// https://github.com/apollographql/graphql-tag/issues/59
-declare module '*.graphql' {
-  import { DocumentNode } from 'graphql';
-
-  const value: DocumentNode;
-  export = value;
-}
